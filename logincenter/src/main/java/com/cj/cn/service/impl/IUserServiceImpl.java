8e018d3b31fb07010ad56b8e64020bdb3055c1e5
xiguanlezz <1561435010@qq.com>
2020-09-24 21:29:58 +0800

    增加专门的登录中心(使用JWT), 正在编写CAS的单点登录

---
+++ logincenter/src/main/java/com/cj/cn/service/impl/IUserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.cj.cn.service.impl;
+
+import com.cj.cn.pojo.User;
+import com.cj.cn.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service("iUserService")
+public class IUserServiceImpl implements IUserService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public User login(String username, String password) {
+        String sql = "SELECT * FROM mmall_user WHERE username=? AND password = ?";
+        Object[] params = new Object[]{username, password};
+        User result = jdbcTemplate.queryForObject(sql, params, (rs, rowNum) -> {
+            User user = new User();
+            user.setId(rs.getInt(1)).setUsername(rs.getString(2)).setPassword(rs.getString(3)).setEmail(rs.getString(4))
+                    .setPhone(rs.getString(5)).setQuestion(rs.getString(6)).setAnswer(rs.getString(7)).setRole(rs.getInt(8));
+            return user;
+        });
+        return result;
+    }
+}
